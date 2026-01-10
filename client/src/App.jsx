08ad22a20502f0c5c243a420c256ad6b11190fbf
imeshaDewmin <dewmin2001@gmail.com>
2026-01-10 19:41:00 +0530

    Add routing and page components for Home, About, Dashboard, SignIn, SignOut, and Projects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../src/pages/Home'
+import About from '../src/pages/About'
+import SignOut from '../src/pages/SignOut'
+import Signin from '../src/pages/Signin'
+import Dashboard from '../src/pages/Dashboard'
+import Projects from '../src/pages/Projects'
+
 export default function App() {
   return (
-    <h1 className="text-red-500 text-5xl">App</h1>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home />}>
+        </Route>
+        <Route path='/about' element={<About />}>
+        </Route>
+        <Route path='/dashboard' element={<Dashboard />}>
+        </Route>
+        <Route path='/signin' element={<Signin />}>
+        </Route>
+        <Route path='/signout' element={<SignOut />}>
+        </Route>
+         <Route path='/projects' element={<Projects />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
