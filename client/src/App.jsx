99baeb5938eb8df732fc2c191fff0bd612df8c0b
imeshaDewmin <dewmin2001@gmail.com>
2026-01-11 12:27:27 +0530

    Create Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignOut from '../src/pages/SignOut'
 import Signin from '../src/pages/Signin'
 import Dashboard from '../src/pages/Dashboard'
 import Projects from '../src/pages/Projects'
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/home' element={<Home />}>
         </Route>
